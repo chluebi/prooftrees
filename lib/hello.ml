79fe07c9254605d1e24fb0c010314858e2146281
chluebi <42419603+chluebi@users.noreply.github.com>
2023-09-03 23:13:40 +0200

    testing around

--- lib/hello.ml
+++ lib/hello.ml
@@ -1,1 +1,13 @@
 let say_hello () = print_endline "Hello, World!"
+
+type colour = 
+ | Red 
+ | Green 
+ | Blue 
+ | Yellow
+ | Mix of color * color
+
+ let test_match = 
+  let a = Mix (Red, Green) in
+  let b = Mix (Red, Blue) in 
+  print_endline "Hello, Match!"
